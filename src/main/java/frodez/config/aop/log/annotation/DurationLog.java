bd3de6e3eaf21382d099d210ee6fb1e22546f1ef
Frodez <463082995@qq.com>
2019-04-13 01:17:13 +0800

    加入耗时监测注解

---
+++ src/main/java/frodez/config/aop/log/annotation/DurationLog.java
@@ -0,0 +1,26 @@
+package frodez.config.aop.log.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 方法耗时监控注解
+ * @author Frodez
+ * @date 2019-01-12
+ */
+@Documented
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DurationLog {
+
+	/**
+	 * 警告触发阈值,默认3000毫秒
+	 * @author Frodez
+	 * @date 2019-04-13
+	 */
+	long threshold() default 3000;
+
+}
