940862a88d68f8f7168876d307ad7579f63f5f7e
Frodez <463082995@qq.com>
2019-05-10 17:35:39 +0800

    重写了AOP部分,加入了加载期检查，并且对一些重要数据进行缓存，减少了反射调用，提高运行速度。

---
+++ src/main/java/frodez/config/aop/log/ParamLogAdvisor.java
@@ -0,0 +1,138 @@
+package frodez.config.aop.log;
+
+import frodez.config.aop.log.annotation.ParamLog;
+import frodez.util.json.JSONUtil;
+import frodez.util.reflect.ReflectUtil;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.aop.Advice;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.aop.MethodMatcher;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.PointcutAdvisor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * 日志管理AOP切面
+ * @author Frodez
+ * @date 2019-01-12
+ */
+@Slf4j
+@Component
+@Order(Integer.MIN_VALUE)
+public class ParamLogAdvisor implements PointcutAdvisor {
+
+	/**
+	 * AOP切点
+	 * @author Frodez
+	 * @date 2019-05-10
+	 */
+	@Override
+	public Advice getAdvice() {
+		/**
+		 * 打印参数到日志
+		 * @param JoinPoint AOP切点
+		 * @author Frodez
+		 * @date 2019-01-12
+		 */
+		return (MethodBeforeAdvice) (method, args, target) -> {
+			Parameter[] parameters = method.getParameters();
+			Map<String, Object> paramMap = new HashMap<>(parameters.length);
+			for (int i = 0; i < parameters.length; ++i) {
+				paramMap.put(parameters[i].getName(), args[i]);
+			}
+			log.info("{} 请求参数:{}", ReflectUtil.getFullMethodName(method), JSONUtil.string(paramMap));
+		};
+	}
+
+	/**
+	 * 默认true
+	 * @author Frodez
+	 * @date 2019-05-10
+	 */
+	@Override
+	public boolean isPerInstance() {
+		return true;
+	}
+
+	/**
+	 * 切入点配置
+	 * @author Frodez
+	 * @date 2019-05-10
+	 */
+	@Override
+	public Pointcut getPointcut() {
+		return new Pointcut() {
+
+			/**
+			 * 根据方法判断
+			 * @author Frodez
+			 * @date 2019-05-10
+			 */
+			@Override
+			public MethodMatcher getMethodMatcher() {
+				return new MethodMatcher() {
+
+					/**
+					 * 对方法进行判断(运行时)
+					 * @author Frodez
+					 * @date 2019-05-10
+					 */
+					@Override
+					public boolean matches(Method method, Class<?> targetClass, Object... args) {
+						if (method.getAnnotation(ParamLog.class) == null) {
+							return false;
+						}
+						if (method.getParameterCount() == 0) {
+							throw new IllegalArgumentException("不能对无参数的方法使用本注解!");
+						}
+						return true;
+					}
+
+					/**
+					 * 对方法进行判断
+					 * @author Frodez
+					 * @date 2019-05-10
+					 */
+					@Override
+					public boolean matches(Method method, Class<?> targetClass) {
+						if (method.getAnnotation(ParamLog.class) == null) {
+							return false;
+						}
+						if (method.getParameterCount() == 0) {
+							throw new IllegalArgumentException("不能对无参数的方法使用本注解!");
+						}
+						return true;
+					}
+
+					/**
+					 * 默认true
+					 * @author Frodez
+					 * @date 2019-05-10
+					 */
+					@Override
+					public boolean isRuntime() {
+						return false;
+					}
+				};
+			}
+
+			/**
+			 * 根据类型判断
+			 * @author Frodez
+			 * @date 2019-05-10
+			 */
+			@Override
+			public ClassFilter getClassFilter() {
+				return clazz -> true;
+			}
+
+		};
+	}
+
+}
---
+++ src/main/java/frodez/config/aop/log/ResultLogAdvisor.java
@@ -0,0 +1,129 @@
+package frodez.config.aop.log;
+
+import frodez.config.aop.log.annotation.ResultLog;
+import frodez.util.json.JSONUtil;
+import frodez.util.reflect.ReflectUtil;
+import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.aop.Advice;
+import org.springframework.aop.AfterReturningAdvice;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.PointcutAdvisor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * 日志管理AOP切面
+ * @author Frodez
+ * @date 2019-01-12
+ */
+@Slf4j
+@Component
+@Order(Integer.MIN_VALUE)
+public class ResultLogAdvisor implements PointcutAdvisor {
+
+	/**
+	 * AOP切点
+	 * @author Frodez
+	 * @date 2019-05-10
+	 */
+	@Override
+	public Advice getAdvice() {
+		/**
+		 * 打印返回值到日志
+		 * @param JoinPoint AOP切点
+		 * @author Frodez
+		 * @date 2019-01-12
+		 */
+		return (AfterReturningAdvice) (returnValue, method, args, target) -> log.info("{} 返回值:{}", ReflectUtil
+			.getFullMethodName(method), JSONUtil.string(returnValue));
+	}
+
+	/**
+	 * 默认true
+	 * @author Frodez
+	 * @date 2019-05-10
+	 */
+	@Override
+	public boolean isPerInstance() {
+		return true;
+	}
+
+	/**
+	 * 切入点配置
+	 * @author Frodez
+	 * @date 2019-05-10
+	 */
+	@Override
+	public Pointcut getPointcut() {
+		return new Pointcut() {
+
+			/**
+			 * 根据方法判断
+			 * @author Frodez
+			 * @date 2019-05-10
+			 */
+			@Override
+			public MethodMatcher getMethodMatcher() {
+				return new MethodMatcher() {
+
+					/**
+					 * 对方法进行判断(运行时)
+					 * @author Frodez
+					 * @date 2019-05-10
+					 */
+					@Override
+					public boolean matches(Method method, Class<?> targetClass, Object... args) {
+						if (method.getAnnotation(ResultLog.class) == null) {
+							return false;
+						}
+						if (method.getReturnType() == Void.class) {
+							throw new IllegalArgumentException("不能对void返回类型的方法使用本注解!");
+						}
+						return true;
+					}
+
+					/**
+					 * 对方法进行判断
+					 * @author Frodez
+					 * @date 2019-05-10
+					 */
+					@Override
+					public boolean matches(Method method, Class<?> targetClass) {
+						if (method.getAnnotation(ResultLog.class) == null) {
+							return false;
+						}
+						if (method.getReturnType() == Void.class) {
+							throw new IllegalArgumentException("不能对void返回类型的方法使用本注解!");
+						}
+						return true;
+					}
+
+					/**
+					 * 默认true
+					 * @author Frodez
+					 * @date 2019-05-10
+					 */
+					@Override
+					public boolean isRuntime() {
+						return false;
+					}
+				};
+			}
+
+			/**
+			 * 根据类型判断
+			 * @author Frodez
+			 * @date 2019-05-10
+			 */
+			@Override
+			public ClassFilter getClassFilter() {
+				return clazz -> true;
+			}
+
+		};
+	}
+
+}
